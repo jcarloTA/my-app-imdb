726ff6df26848c2c1a740307c2c93a44d334e5d9
Jancarlo <janta1696@gmail.com>
2020-01-10 13:25:44 -0600

    edit config on enviroment production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   API_KEY: '86e10db2ccd9285a3fcd2b48f5db3c4b',
-  API_URL: 'https://api.themoviedb.org/3',
+  API_URL: 'https://api.themoviedb.org/',
   API_URL_AUTH: 'https://www.themoviedb.org',
   API_URL_IMAGES: 'https://image.tmdb.org',
   LANGUAGE: 'es-GT',
